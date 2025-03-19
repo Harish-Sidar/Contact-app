dfc3027444d5452ed38c5a7e02827d90b9cd8dd2
hk265 <Nimcet9900@gmail.com>
2025-03-19 18:35:18 +0530

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,49 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import { IoSearch } from "react-icons/io5";
+import { CiCirclePlus } from "react-icons/ci";
+import { collection } from 'firebase/firestore'
+import { db } from './Config/Firebase'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
+
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    const getContacts = async () =>{
+      try {
+
+        const contactsRef = collection(db,"contacts")
+        const contactsSnapshot = await getDocs(contactsRef)
+        console.log(contactsSnapshot)
+        
+      } catch (error) {
+        
+      }
+    }
+
+    getContacts();
+  
+ 
+  }, [])
+  
 
   return (
     <>
+    <div className='mx-auto max-w-[370px] px-4'>
     <Navbar/>
+    <div className='flex gap-2'>
+      <div className='relative flex items-center flex-grow'>
+    <IoSearch className=' absolute ml-1 text-3xl text-white' />
+      <input type="text" className='h-10 flex-grow rounded-md border
+       border-white bg-transparent text-white pl-9 ' />
+    </div> 
+    <CiCirclePlus className='text-5xl cursor-pointer text-white' />
+
+    
+    </div>
+    </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='m-4 h-[60px] bg-white rounded-lg'> Navbar
+    <div className='m-4 flex h-[60px] bg-white rounded-lg items-center 
+    justify-center text-xl font-medium'> 
       
-    </div>
+        <img src="/firebase.svg" alt="" />
+        <h1>Firebase Contact App</h1>
+      </div>
+
+    
   )
 }
 
